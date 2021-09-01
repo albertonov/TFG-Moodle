8e9a9026da7957955c787701146059a4bd88676b
Alberto <albertonov98@gmail.com>
2021-09-01 22:15:44 +0200

    Travis fixed error for test

---
+++ scriptSQLTravis.sql
@@ -0,0 +1,40 @@
+--Añadido campo de experiencia en mdl_user
+SELECT * FROM information_schema.tables;
+
+ALTER TABLE p_user ADD totalexperience int8 NULL DEFAULT 0;
+ALTER TABLE p_user ADD totalexperience int8 NULL DEFAULT 0;
+
+--Añadido campo de experiencia en mdl_user_enrolments
+
+ALTER TABLE p_user_enrolments ADD courseexperience int8 NULL DEFAULT 0;
+ALTER TABLE p_user_enrolments ADD courseexperience int8 NULL DEFAULT 0;
+
+-- Añadido campo multiplicador en Assign
+ALTER TABLE p_assign ADD isgamebased boolean NOT NULL DEFAULT false;
+ALTER TABLE p_assign ADD multiplicadorgb float4 NULL DEFAULT 1.00;
+
+ALTER TABLE p_assign ADD isgamebased boolean NOT NULL DEFAULT false;
+ALTER TABLE p_assign ADD multiplicadorgb float4 NULL DEFAULT 1.00;
+
+-- Añadida nueva tabla mdl_post_qualifications para calificar los posts
+
+CREATE TYPE qualification AS ENUM ('positive', 'negative', 'like');
+
+CREATE TABLE mdl_post_qualifications (
+	id serial PRIMARY KEY,  
+	id_post integer REFERENCES p_forum_posts (id),
+	id_user integer REFERENCES p_user (id),
+	qual qualification
+);
+
+
+
+
+CREATE TABLE phpu_post_qualifications (
+	id serial PRIMARY KEY,  
+	id_post integer REFERENCES p_forum_posts (id),
+	id_user integer REFERENCES p_user (id),
+	qual qualification
+);
+
+
